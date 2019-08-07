6c4123786cfc55481634bb86edb95b7f38635dba
Nikhil <nbalyan10@gmail.com>
2019-08-07 23:04:26 +0530

    initialize project for different types of mapping

---
+++ src/main/java/mapping/hibernate/mapping_hibernate/App.java
@@ -0,0 +1,13 @@
+package mapping.hibernate.mapping_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
